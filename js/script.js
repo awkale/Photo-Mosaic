2608b826f58f8723c6a9e9782e15a5d97f0e92ac
awkale <hi@awkale.me>
2013-10-06 13:02:10 -0400

    styling

--- js/script.js
+++ js/script.js
@@ -1,6 +1,7 @@
 $(document).ready(function(){
-	
+
 	var key = 'dc2498ad3e413bde8dc546a8013c3736';
+
 
 	$('#search-form').submit(function(e){
 		//by default, submitting a form reloads the page. "e" represents the
@@ -9,7 +10,8 @@ $(document).ready(function(){
 		e.preventDefault();
 
 		// change the value below to whatever happens to be in the search box
-		var text = null;
+		var text = $('#keywords').val();
+
 
 		//prevent the user from searching unless there is text in the text box
 		//if() {
@@ -20,8 +22,8 @@ $(document).ready(function(){
 		//docs for the photos.search method to see which parameters you need to add to
 		//the URL to accomplish this
 
-		$.getJSON('http://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&per_page=50&content_type=1&jsoncallback=?', successCallback);
-		
+		$.getJSON('http://api.flickr.com/services/rest/?method=flickr.photos.search&format=json&per_page=50&content_type=1&jsoncallback=?&api_key='+key+'&text='+text, successCallback);
+
 		// } else {
 			//create an alert box that lets the user know he/she needs to enter text
 		//}
@@ -29,28 +31,34 @@ $(document).ready(function(){
 
 	function successCallback(data) {
 		//log the variable "data" to the console to see what the object looks like
+		console.log(data);
 
 		//now loop through the photos and add markup to the DOM. Read the
 		//directions.txt doc included in the project for guidance. Grab the "q"
 		//version of each photo.
-		var photos = null;
+		var photos = data.photos.photo;
+
 
 		//initialize the html string so we can build on it in the loop
 		var markup = '';
 
-		//assemble your markup 
-		//for() {
+		//assemble your markup
+		for(var i=0; i < photos.length; i++) {
 			//construct the photo url
-			
+
+			var url = 'http://farm'+photos[i].farm+'.staticflickr.com/'+photos[i].server+'/'+photos[i].id+'_'+photos[i].secret+'_q.jpg';
 			//log it to the console and try to visit
 			//it in the browser to make sure it works
-			
+			console.log(url);
 			//add a div with a class of "tile" and your new image tag to the markup string
-			
-		//}
+
+      markup += '<div class="tile"><img src="' + url + '"/></div>';
+
+		}
+
+		$('#tiles').html(markup);
 
 		//replace any existing markup in #tiles with the new markup we just created
-		
 	}
 
 });
